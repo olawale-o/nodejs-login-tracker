f0973c6cc038cf6997273628d4d66315956fed82
olawale-o <omoogunolawale@gmail.com>
2023-03-17 02:47:53 +0100

    Create auth handler

---
+++ src/auth/handler.js
@@ -0,0 +1,39 @@
+const db = require("../models");
+const { hashPassword, comparePassword } = require("../helpers/password-helper");
+
+exports.register = async (req, res) => {
+  try {
+    const { fullName, email, password } = req.body;
+
+    const isFound = await db.User.findOne({
+      where: { email }
+    });
+  
+    if (isFound) {
+      return res.status(400).json({
+        message: "Email already exists. Please login with your credentials."
+      });
+    }
+    const hashedPassword = await hashPassword(password);
+  
+    const user = await db.User.create({
+      full_naame: fullName,
+      email,
+      password: hashedPassword
+    });
+  
+    if (!user) {
+      return res.status(500).json({
+        message: "Something went wrong. Please try again."
+      });
+    }
+    console.log(user);
+    res.status(201).json({
+      message: "User created successfully."
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.login = async (req, res) => {};
