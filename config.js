bab6d9f55f29ca4d51c4206ad014ab1dff8b367e
olawale-o <omoogunolawale@gmail.com>
2023-03-17 03:25:57 +0100

    Create config file for convict setup

---
+++ config.js
@@ -0,0 +1,54 @@
+const convict = require('convict');
+const convict_format_with_validator = require('convict-format-with-validator');
+
+// convict.addFormat(convict_format_with_validator.port);
+
+const config = convict({
+  env: {
+    doc: 'The application environment.',
+    format: ['production', 'development', 'test'],
+    default: 'development',
+    env: 'NODE_ENV',
+  },
+  port: {
+    doc: 'The port to bind.',
+    format: 'port',
+    default: 5000,
+    env: 'PORT',
+    arg: 'port',
+  },
+  db: {
+    username: {
+      doc: 'Database username',
+      format: String,
+      default: '',
+    },
+    password: {
+      doc: 'Database password',
+      format: String,
+      default: null,
+    },
+    dialect: {
+      doc: 'Database dialect',
+      format: String,
+      default: 'postgres',
+    },
+    name: {
+      doc: 'Database name',
+      format: String,
+      default: 'auth_tracker',
+    },
+    port: {
+      doc: 'Database port',
+      format: 'port',
+      default: 5432,
+    }
+  },
+});
+
+const env = config.get('env');
+config.loadFile(`./src/config/${env}.json`);
+
+config.validate({ allowed: 'strict' });
+
+module.exports = config;
