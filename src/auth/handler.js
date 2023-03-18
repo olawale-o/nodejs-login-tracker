7b99870deee7475964cb7f5f09fa95d3de204d8d
olawale-o <omoogunolawale@gmail.com>
2023-03-18 02:11:46 +0100

    Track user login

--- src/auth/handler.js
+++ src/auth/handler.js
@@ -1,3 +1,4 @@
+const moment = require("moment");
 const db = require("../models");
 const { hashPassword, comparePassword } = require("./helpers/password-helper");
 
@@ -36,4 +37,57 @@ exports.register = async (req, res) => {
   }
 };
 
-exports.login = async (req, res) => {};
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const isFound = await db.User.findOne({
+      where: { email }
+    });
+    if (!isFound) {
+      return res.status(400).json({
+        message: "Invalid email or password."
+      });
+    }
+    const isPasswordValid = await comparePassword(password, isFound.password);
+
+    if (!isPasswordValid) {
+      if (isFound.login_attempts >= 3) {
+        await db.User.update({
+          is_locked: true,
+          next_unlock_time: moment().add(1, "minutes").valueOf()
+        }, {where: {email}});
+        return res.status(400).json({
+          message: "Your account has been locked for 1 minute.",
+        });
+      } else {
+        await db.User.update({
+          login_attempts: isFound.login_attempts + 1,
+        }, {where: {email}});
+        return res.status(400).json({
+          message: "Invalid email or password.",
+        });
+      }
+    } else {
+      if (isFound?.next_unlock_time >= moment().valueOf()) {
+        await db.User.update({
+          next_unlock_time: moment().add(1, "minutes").valueOf(),
+        }, {where: {email}});
+        return res.status(400).json({
+          message: "Your account has been locked for another 1 minute.",
+        });
+      } else {
+        await db.User.update({
+          login_attempts: 0,
+          is_locked: false,
+          next_unlock_time: null
+        }, {where: {email}});
+      }
+    }
+    return res.status(200).json({
+      message: "Login successful.",
+    });
+
+  } catch (error) {
+    console.log(error);
+  }
+};
