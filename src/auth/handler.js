b46d08128effa043ddbb31cd8db4b0404263c5a4
olawale-o <omoogunolawale@gmail.com>
2023-03-18 03:12:45 +0100

    Fix login bug

--- src/auth/handler.js
+++ src/auth/handler.js
@@ -50,6 +50,42 @@ exports.login = async (req, res) => {
     }
     const isPasswordValid = await comparePassword(password, isFound.password);
 
+    if (isPasswordValid === false &&
+        isFound.is_locked &&
+        moment().isBefore(isFound.next_unlock_time)
+      ) {
+      return res.status(400).json({
+        message: "Your account will be locked for life.",
+      });
+    }
+
+    if (isPasswordValid === false &&
+        isFound.is_locked &&
+        moment().isAfter(isFound.next_unlock_time)
+      ) {
+      isFound.login_attempts = 0;
+      await db.User.update({
+        is_locked: false,
+        next_unlock_time: null,
+        login_attempts: 0
+      }, {where: {email}});
+    }
+
+    if (isPasswordValid && isFound.is_locked && moment().isAfter(isFound.next_unlock_time)) {
+      isFound.login_attempts = 0;
+      await db.User.update({
+        is_locked: false,
+        next_unlock_time: null,
+        login_attempts: 0
+      }, {where: {email}});
+    }
+
+    if (isPasswordValid && isFound.is_locked && moment().isBefore(isFound.next_unlock_time)) {
+      return res.status(400).json({
+        message: "You cannot login at the momment.",
+      });
+    }
+
     if (!isPasswordValid) {
       if (isFound.login_attempts >= 3) {
         await db.User.update({
@@ -67,22 +103,14 @@ exports.login = async (req, res) => {
           message: "Invalid email or password.",
         });
       }
-    } else {
-      if (isFound?.next_unlock_time >= moment().valueOf()) {
-        await db.User.update({
-          next_unlock_time: moment().add(1, "minutes").valueOf(),
-        }, {where: {email}});
-        return res.status(400).json({
-          message: "Your account has been locked for another 1 minute.",
-        });
-      } else {
-        await db.User.update({
-          login_attempts: 0,
-          is_locked: false,
-          next_unlock_time: null
-        }, {where: {email}});
-      }
     }
+    await db.User.update({
+      login_attempts: 0,
+      is_locked: false,
+      next_unlock_time: null
+    }, {where: {email}});
+    
+    
     return res.status(200).json({
       message: "Login successful.",
     });
