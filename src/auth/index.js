da5b765e859bdfbb308af4ef2fb189fb8d1dc363
olawale-o <omoogunolawale@gmail.com>
2023-03-19 00:59:31 +0100

    Use validation middleware

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
+const {
+  validateRequestSchema,
+  registerSchema,
+  loginSchema,
+} = require('./validations/auth.validation');
 
 const handler = require('./handler');
 
-router.post('/register', handler.register);
-router.post('/login', handler.login);
+router.post('/register', validateRequestSchema(registerSchema), handler.register);
+router.post('/login', validateRequestSchema(loginSchema), handler.login);
 
 module.exports = router;
