3c9bba344ab9eed06f1a1defe1b2ebad98878a1f
olawale-o <omoogunolawale@gmail.com>
2023-03-19 02:13:20 +0100

    Handle error at app level

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyPaser = require("body-parser");
 const routeHandler = require("./router-handler");
+const handleError = require("./common/error-handler");
 const app = express();
 
 app.use(bodyPaser.urlencoded({ extended: true }))
@@ -12,6 +13,14 @@ app.use(bodyPaser.urlencoded({extended: true}));
 app.use(bodyPaser.json());
 
 routeHandler(app);
+
+app.use(async (err, _req, res, _next) => {
+  await handleError(err, res);
+});
+
+// process.on('uncaughtException', (error) => {
+//   handleError(error)
+// })
 
 //
 module.exports = app;
