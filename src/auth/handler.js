bde53b69dbc598cde4ee9fc20d3af710076bca9b
olawale-o <omoogunolawale@gmail.com>
2023-03-19 04:54:21 +0100

    Use login service

--- src/auth/handler.js
+++ src/auth/handler.js
@@ -1,8 +1,5 @@
-const moment = require("moment");
-const db = require("../models");
-const { register } = require("./service");
+const { register, login } = require("./service");
 const transformResponse = require("../common/transform-response");
-const { comparePassword } = require("./helpers/password-helper");
 
 exports.register = async (req, res, next) => {
   try {
@@ -19,87 +16,7 @@ exports.register = async (req, res, next) => {
 
 exports.login = async (req, res, next) => {
   try {
-    const { email, password } = req.body;
-    const isFound = await db.User.findOne({
-      where: { email }
-    });
-    if (!isFound) {
-      transformResponse({
-        statusCode: 400,
-        res,
-        message: "Invalid email or password"
-      });
-    }
-    const isPasswordValid = await comparePassword(password, isFound.password);
-
-    if (isPasswordValid === false &&
-        isFound.is_locked &&
-        moment().isBefore(isFound.next_unlock_time)
-      ) {
-        transformResponse({
-        statusCode: 400,
-        res,
-        message: "Your account will be locked for life"
-      });
-    }
-
-    if (isPasswordValid === false &&
-        isFound.is_locked &&
-        moment().isAfter(isFound.next_unlock_time)
-      ) {
-      isFound.login_attempts = 0;
-      await db.User.update({
-        is_locked: false,
-        next_unlock_time: null,
-        login_attempts: 0
-      }, {where: {email}});
-    }
-
-    if (isPasswordValid && isFound.is_locked && moment().isAfter(isFound.next_unlock_time)) {
-      isFound.login_attempts = 0;
-      await db.User.update({
-        is_locked: false,
-        next_unlock_time: null,
-        login_attempts: 0
-      }, {where: {email}});
-    }
-
-    if (isPasswordValid && isFound.is_locked && moment().isBefore(isFound.next_unlock_time)) {
-      transformResponse({
-        statusCode: 400,
-        res,
-        message: "You cannot login at the momment"
-      })
-    }
-
-    if (!isPasswordValid) {
-      if (isFound.login_attempts >= 3) {
-        await db.User.update({
-          is_locked: true,
-          next_unlock_time: moment().add(1, "minutes").valueOf()
-        }, {where: {email}});
-        transformResponse({
-          statusCode: 400,
-          res,
-          message: "Your account has been locked for 1 minute"
-        });
-      } else {
-        await db.User.update({
-          login_attempts: isFound.login_attempts + 1,
-        }, {where: {email}});
-        transformResponse({
-          statusCode: 400,
-          res,
-          message: "Invalid email or password"
-        })
-      }
-    }
-    await db.User.update({
-      login_attempts: 0,
-      is_locked: false,
-      next_unlock_time: null
-    }, {where: {email}});
-    
+    await login(req.body);
     transformResponse({
       statusCode: 200,
       res,
