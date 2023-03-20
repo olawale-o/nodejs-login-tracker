9e7c35b6678b56c749a38ab3fdab46234cd2fd2a
olawale-o <omoogunolawale@gmail.com>
2023-03-20 09:35:41 +0100

    Add more tests

--- src/__tests__/login.test.js
+++ src/__tests__/login.test.js
@@ -5,12 +5,24 @@ const db = require('../models');
 
 describe('POST /login', () => {
   beforeAll(async () => {
-    const data = {
-      full_name: 'test test',
-      email: 'test@test.com',
-      password: await hashPassword('password'),
-    };
-    await db.User.create(data);
+    const datas = [
+      {
+        full_name: 'test test',
+        email: 'test@test.com',
+        password: await hashPassword('password'),
+      },
+      {
+        full_name: 'joy doe',
+        email: 'joy@test.com',
+        password: await hashPassword('password'),
+      },
+      {
+        full_name: 'wale doe',
+        email: 'wale@test.com',
+        password: await hashPassword('password'),
+      },
+    ];
+    await db.User.bulkCreate(datas);
   });
 
 
@@ -113,4 +125,72 @@ describe('POST /login', () => {
         done();
       });
     });
+
+   test('should not login with invalid password', (done) => {
+    const data = {
+      email: 'test@test.com',
+      password: 'passwords',
+    };
+    request(app)
+      .post('/api/v1/auth/login')
+      .set('Content-Type', 'application/json')
+      .send(data)
+      .end((err, res) => {
+        expect(res.statusCode).toEqual(400);
+        expect(res.body).toHaveProperty('message', 'Invalid email or password');
+        if (err) return done(err);
+        done();
+      });
+    });
+
+   test('should lock account for 1 minute after trying to login 4 times', async () => {
+    const data = {
+      email: 'joy@test.com',
+      password: 'passwords',
+    };
+    const status = [];
+    for (let i = 0; i < 4; i++) {
+      const response = await request(app)
+        .post('/api/v1/auth/login')
+        .set('Content-Type', 'application/json')
+        .send(data);
+      status.push({
+        statusCode: response.statusCode,
+        message: response.body.message,
+      });
+    }
+    expect(status).toHaveLength(4);
+    expect(status).toEqual([
+      { statusCode: 400, message: 'Invalid email or password' },
+      { statusCode: 400, message: 'Invalid email or password' },
+      { statusCode: 400, message: 'Invalid email or password' },
+      { statusCode: 400, message: 'Your account has been locked for 1 minute' },
+    ]);
+  });
+
+  test('should lock account permanently after 4 tries and attempting to login before 1 minute grace period', async () => {
+    const data = {
+      email: 'wale@test.com',
+      password: 'passwords',
+    };
+    const status = [];
+    for (let i = 0; i < 5; i++) {
+      const response = await request(app)
+        .post('/api/v1/auth/login')
+        .set('Content-Type', 'application/json')
+        .send(data);
+      status.push({
+        statusCode: response.statusCode,
+        message: response.body.message,
+      });
+    }
+    expect(status).toHaveLength(5);
+    expect(status).toEqual([
+      { statusCode: 400, message: 'Invalid email or password' },
+      { statusCode: 400, message: 'Invalid email or password' },
+      { statusCode: 400, message: 'Invalid email or password' },
+      { statusCode: 400, message: 'Your account has been locked for 1 minute' },
+      { statusCode: 400, message: 'Your account will be locked for life' },
+    ]);
+  });
 });
