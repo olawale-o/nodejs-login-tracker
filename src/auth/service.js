00ee2327c5a157e2e3edcdcd84c5c43513846a01
olawale-o <omoogunolawale@gmail.com>
2024-11-25 16:04:38 +0100

    Review login logic

--- src/auth/service.js
+++ src/auth/service.js
@@ -1,26 +1,32 @@
-const moment = require('moment');
-const db = require('../models');
-const AppError = require('../common/app-error');
-const { hashPassword, comparePassword } = require('./helpers/password-helper');
+const moment = require("moment");
+const db = require("../models");
+const AppError = require("../common/app-error");
+const { hashPassword, comparePassword } = require("./helpers/password-helper");
 
 const register = async (credentials) => {
   const { email, fullName, password } = credentials;
   const isFound = await db.User.findOne({
-    where: { email }
+    where: { email },
   });
   if (isFound) {
-    throw new AppError(400, "Email already exists. Please login with your credentials");
+    throw new AppError(
+      400,
+      "Email already exists. Please login with your credentials",
+    );
   }
   const hashedPassword = await hashPassword(password);
-  
+
   const user = await db.User.create({
     full_name: fullName,
     email,
-    password: hashedPassword
+    password: hashedPassword,
   });
 
   if (!user) {
-    throw new AppError(500, "Something went wrong. Could not create user at this time. Please try again later");
+    throw new AppError(
+      500,
+      "Something went wrong. Could not create user at this time. Please try again later",
+    );
   }
   return user;
 };
@@ -28,63 +34,50 @@ const register = async (credentials) => {
 const login = async (credentials) => {
   const { email, password } = credentials;
   const isFound = await db.User.findOne({
-    where: { email }
+    where: { email },
   });
   if (!isFound) {
     throw new AppError(400, "Invalid email or password");
   }
+
+  if (isFound.next_unlock_time && moment().isBefore(isFound.next_unlock_time)) {
+    throw new AppError(400, "Your account is still locked");
+  }
+
   const isPasswordValid = await comparePassword(password, isFound.password);
-  if (isPasswordValid === false &&
-      isFound.is_locked &&
-      moment().isBefore(isFound.next_unlock_time)
-    ) {
-      throw new AppError(400, "Your account will be locked for life");
-    }
-
-  if (isPasswordValid === false &&
-      isFound.is_locked &&
-      moment().isAfter(isFound.next_unlock_time)
-    ) {
-      isFound.login_attempts = 0;
-      await db.User.update({
-        is_locked: false,
-        next_unlock_time: null,
-        login_attempts: 0
-      }, {where: {email}});
-    }
-
-  if (isPasswordValid && isFound.is_locked && moment().isAfter(isFound.next_unlock_time)) {
-    isFound.login_attempts = 0;
-    await db.User.update({
-      is_locked: false,
-      next_unlock_time: null,
-      login_attempts: 0
-    }, {where: {email}});
-  }
-
-  if (isPasswordValid && isFound.is_locked && moment().isBefore(isFound.next_unlock_time)) {
-    throw new AppError(400, "You cannot login at the momment");
-  }
 
   if (!isPasswordValid) {
     if (isFound.login_attempts >= 3) {
-      await db.User.update({
-        is_locked: true,
-        next_unlock_time: moment().add(1, "minutes").valueOf()
-      }, {where: {email}});
-      throw new AppError(400, "Your account has been locked for 1 minute");
-    } else {
-      await db.User.update({
-        login_attempts: isFound.login_attempts + 1,
-      }, {where: {email}});
-      throw new AppError(400, "Invalid email or password");
+      await db.User.update(
+        {
+          is_locked: true,
+          next_unlock_time: moment().add(1, "minutes").valueOf(),
+        },
+        { where: { email } },
+      );
+      throw new AppError(400, "You cannot login at this time");
     }
+    isFound.login_attempts += 1;
+    await db.User.update(
+      {
+        login_attempts: isFound.login_attempts,
+      },
+      { where: { email } },
+    );
+    throw new AppError(
+      400,
+      "Your password is invalid, you have few attempts to try",
+    );
   }
-  await db.User.update({
-    login_attempts: 0,
-    is_locked: false,
-    next_unlock_time: null
-  }, {where: {email}});
+
+  await db.User.update(
+    {
+      login_attempts: 0,
+      is_locked: false,
+      next_unlock_time: null,
+    },
+    { where: { email } },
+  );
 };
 
 module.exports = {
