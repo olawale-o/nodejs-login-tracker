b3345afab291fcc8f8a3a91bcba00b5f740c8abd
olawale-o <omoogunolawale@gmail.com>
2024-11-25 16:09:11 +0100

    Modify user model

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -13,37 +11,44 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  User.init({
-    full_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  User.init(
+    {
+      fullName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: "full_name",
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      loginAttempts: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        field: "login_attempts",
+      },
+      isLocked: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+        field: "is_locked",
+      },
+      nextUnlockTime: {
+        type: DataTypes.DATE,
+        allowNull: true,
+        field: "next_unlock_time",
+      },
     },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
+    {
+      sequelize,
+      modelName: "User",
+      tableName: "users",
     },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    login_attempts: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0
-    },
-    is_locked: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false
-    },
-    next_unlock_time: {
-      type: DataTypes.DATE,
-      allowNull: true
-    },
-  }, {
-    sequelize,
-    modelName: 'User',
-    tableName: 'users'
-  });
+  );
   return User;
 };
