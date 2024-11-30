fcf15bd0f07dbcda7df8bdf9d05c105b6f807d85
olawale-o <omoogunolawale@gmail.com>
2024-11-30 11:37:37 +0100

    Update user model with association

--- src/models/user.js
+++ src/models/user.js
@@ -9,10 +9,22 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      User.hasMany(models.Token, {
+        as: "my_tokens",
+        foreignKey: {
+          name: "user_id",
+        },
+      });
     }
   }
   User.init(
     {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
       fullName: {
         type: DataTypes.STRING,
         allowNull: false,
