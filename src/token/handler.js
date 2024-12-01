529a5c43208da4dc6e5d4c8eb93a920f8e1387bb
olawale-o <omoogunolawale@gmail.com>
2024-12-01 01:18:37 +0100

    Use auth-token module

--- src/token/handler.js
+++ src/token/handler.js
@@ -1,4 +1,7 @@
-const { accessToken, refreshToken } = require("../auth/helpers/jwt");
+const {
+  generateAccessToken,
+  generateRefreshToken,
+} = require("../auth/helpers/auth-token");
 const AppError = require("../common/app-error");
 const verifyToken = require("../common/verify-token");
 const db = require("../models");
@@ -22,17 +25,20 @@ exports.refresh = async (req, res, next) => {
       throw new AppError(403, "Token verification failed");
     }
 
-    const tokenAccess = accessToken({ id: decoded.id }, "ACCESS_TOKEN");
-    const tokenRefresh = refreshToken({ id: decoded.id }, "REFRESH_TOKEN");
+    const accessToken = generateAccessToken({ id: decoded.id }, "ACCESS_TOKEN");
+    const refreshToken = generateRefreshToken(
+      { id: decoded.id },
+      "REFRESH_TOKEN",
+    );
 
     await db.Token.create({
-      accessToken: tokenAccess,
-      refreshToken: tokenRefresh,
+      accessToken,
+      refreshToken,
       user_id: isFound.user_id,
     });
 
-    res.cookie("accessToken", tokenAccess, { httpOnly: true, secure: true });
-    res.cookie("refreshToken", tokenRefresh, { httpOnly: true, secure: true });
+    res.cookie("accessToken", accessToken, { httpOnly: true, secure: true });
+    res.cookie("refreshToken", refreshToken, { httpOnly: true, secure: true });
     return res.status(200).json({ message: "Token refreshed" });
   } catch (e) {
     next(e);
