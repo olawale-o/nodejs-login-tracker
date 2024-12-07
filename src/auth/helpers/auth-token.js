14525b37bb00e752a03c15046c2c91a694a1ef13
olawale-o <omoogunolawale@gmail.com>
2024-12-07 07:05:16 +0100

    Add generateJWTToken and generateAccessTokenWithUserIdAndVersionId

--- src/auth/helpers/auth-token.js
+++ src/auth/helpers/auth-token.js
@@ -3,13 +3,19 @@ const { signToken } = require("../../common/jwt");
 const ACCESS_TOKEN_EXPIRATION = 60 * 30;
 const REFRESH_TOKEN_EXPIRATION = 60 * 60;
 
+const generateJWTToken = (data, key, expires) => signToken(data, key, expires);
+
 const generateAccessToken = (data) =>
-  signToken(data, "ACCESS_TOKEN", ACCESS_TOKEN_EXPIRATION);
+  generateJWTToken(data, "ACCESS_TOKEN", ACCESS_TOKEN_EXPIRATION);
 
 const generateRefreshToken = (data) =>
-  signToken(data, "REFRESH_TOKEN", REFRESH_TOKEN_EXPIRATION);
+  generateJWTToken(data, "REFRESH_TOKEN", REFRESH_TOKEN_EXPIRATION);
+
+const generateAccessTokenWithUserIdAndVersionId = (data) =>
+  generateJWTToken(data, "TOKEN_VERSION", ACCESS_TOKEN_EXPIRATION);
 
 module.exports = {
   generateAccessToken,
   generateRefreshToken,
+  generateAccessTokenWithUserIdAndVersionId,
 };
