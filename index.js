8049034cf56412d784b6ddf31cf39488ccf857cd
olawale-o <omoogunolawale@gmail.com>
2025-05-03 16:10:05 +0100

    fix: lint error

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 const db = require("./src/models");
-const app = require('./src/app');
-const config = require('./config');
-const port = config.get('port');
-const env = config.get('env');
+const app = require("./src/app");
+const config = require("./config");
+const port = config.get("port");
+const env = config.get("env");
 
-db.sequelize.sync()
+db.sequelize
+  .sync()
   .then(() => {
-    if (env !== 'test') {
+    if (env !== "test") {
       console.log("Synced db.");
     }
   })
   .catch((err) => {
-    if (env !== 'test') {
-      console.log("Failed to sync db: " + err.message);
+    if (env !== "test") {
+      console.log(`Failed to sync db: ${err.message}`);
     }
-  })
+  });
 
-if (env !== 'test') {
+if (env !== "test") {
   app.listen(port, () => {
     console.log(`Running on port ${port}`);
   });
